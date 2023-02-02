dc686681a3d9ad7df83bf6a2d02e76c5562e863a
Fnu Ashmika <ashmika.bv@gmail.com>
2023-02-02 09:30:19 -0600

    added choice

--- use_created_module.py
+++ use_created_module.py
@@ -10,11 +10,15 @@
 from module import area_house , circle
 # calculate the circumference of a circle
 #Formated it to show the result upto 3 decimal places
-radius = float(input ('Enter circle radius: '))
-print(f'Circumference of the circle is: {circle(radius):.3f}')
+ques= input('What do you want to calculate-Circumference or Area? Enter(c/a): ')
+if ques == 'c' :
+    radius = float(input ('Enter circle radius: '))
+    print(f'Circumference of the circle is: {circle(radius):.3f}')
+elif ques == 'a' :
+    # calculate the square footage of a house
+    #Formated it to show the result upto 3 decimal places
+    length = float(input('Enter length of your house: '))
+    width = float(input('Enter width of your house: '))
+    print(f'Area of your house is: {area_house(length,width):.3f}')
 
-# calculate the square footage of a house
-#Formated it to show the result upto 3 decimal places
-length = float(input('Enter length of your house: '))
-width = float(input('Enter width of your house: '))
-print(f'Area of your house is: {area_house(length,width):.3f}')
+    
